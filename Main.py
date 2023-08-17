17030d330ca50f2e5631893fa5ab1918b68b63fe
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-17 18:39:07 -0400

    setting FPS

--- Main.py
+++ Main.py
@@ -10,6 +10,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Dino Fighter")
 
+# set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
 # load background image
 bg_image = pygame.image.load("assets/images/background/bg.jpg").convert_alpha()
 
