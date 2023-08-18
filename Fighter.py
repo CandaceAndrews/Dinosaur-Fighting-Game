33be09e4ade4795538455c4734c907b7c6c0220a
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-18 17:39:48 -0400

    starting attack logic

--- Fighter.py
+++ Fighter.py
@@ -6,6 +6,7 @@ class Fighter():
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attack_type = 0
 
     def move(self, screen_width, screen_height):
         SPEED = 10
@@ -27,6 +28,14 @@ class Fighter():
             self.vel_y = -30
             self.jump = True
 
+        # attack
+        if key[pygame.K_r] or key[pygame.K_t]:
+            # determine which attack type was used
+            if key[pygame.K_r]:
+                self.attack_type = 1
+            if key[pygame.K_t]:
+                self.attack_type = 2
+
         # apply gravity
         self.vel_y += GRAVITY
         dy += self.vel_y
@@ -44,6 +53,9 @@ class Fighter():
         # update player position
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self):
+        pass
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
